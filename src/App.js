c18e2ac47d9da1dd7e7194d3baa8b56160fea10f
Louis <>
2020-12-03 18:05:38 +0000

    complete tutorial 2.1 - 2.3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 // import Setup1 from "./tutorial/1-useState/setup/1-error-example";
-// import Final1 from "./tutorial/1-useState/final/1-error-example";
 // import Setup2 from "./tutorial/1-useState/setup/2-useState-basics";
-// import Final2 from "./tutorial/1-useState/final/2-useState-basics";
 // import Setup3 from "./tutorial/1-useState/setup/3-useState-array";
-// import Final3 from "./tutorial/1-useState/final/3-useState-array";
 // import Setup4 from "./tutorial/1-useState/setup/4-useState-object";
-// import Final4 from "./tutorial/1-useState/final/4-useState-object";
-import Setup5 from "./tutorial/1-useState/setup/5-useState-counter";
-import Final5 from "./tutorial/1-useState/final/5-useState-counter";
+// import Setup5 from "./tutorial/1-useState/setup/5-useState-counter";
+// import Setup6 from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+// import Setup7 from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+import Setup8 from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
-      <Setup5 />
+      <Setup8 />
     </div>
   );
 }
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const checkSize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+  return (
+    <>
+      <h1>Window Dimensions</h1>
+      <br />
+      <h2>Width</h2>
+      <h3>{width}px</h3>
+      <h2>Height</h2>
+      <h3>{height}px</h3>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
