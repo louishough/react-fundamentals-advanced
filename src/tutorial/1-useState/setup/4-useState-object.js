1e130f54c244ec3b7aa282e603f922409e7ae67c
Louis <>
2020-12-03 11:36:59 +0000

    Complete 1.4 and 1.5

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "Message String",
+  });
+
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("Message String");
+
+  const changeMessage = () => {
+    if (name === "Peter") {
+      setMessage("Hello World!");
+      setAge(29);
+      setName("Bobby");
+    } else {
+      setMessage("Message String");
+      setAge(24);
+      setName("Peter");
+    }
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const decreaseValue = () => {
+    setValue(value - 1);
+  };
+
+  const resetValue = () => {
+    setValue(0);
+  };
+
+  const increaseValue = () => {
+    setValue(value + 1);
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={decreaseValue}>
+          Decrease
+        </button>
+        <button className="btn" onClick={resetValue}>
+          Reset
+        </button>
+        <button className="btn" onClick={increaseValue}>
+          Increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button
+          className="btn"
+          onClick={() => {
+            setTimeout(() => {
+              setValue((prevState) => {
+                return prevState + 1;
+              });
+            }, 2000);
+          }}
+        >
+          Increase in two Seconds
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
